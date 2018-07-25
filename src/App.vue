9984099689b9165461c2caff4474b089fa58d1bf
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-07-25 23:19:40 +1000

    header and nav component + stylus files
    add

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,13 +9,26 @@
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
 <style lang="stylus">
+@import './styles/typography.styl'
+
 body, html
   margin 0
   padding 0
 
 #app
-  font-family 'IBM Plex Sans', 'Avenir', Helvetica, Arial, sans-serif
+  // font-family 'IBM Plex Sans', 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   // text-align center
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div class="header-inner">
+      <h1>Ramkumar Shankar</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header-inner
+  padding: 0px 100px
+  display: flex
+  align-items: center
+  justify-content: space-between
+
+  h1
+    font-size: 1.414em
+
+ul
+  display: inline-block
+  list-style-type: none
+
+  li
+    display: inline
+    padding-left: 20px
+    
+    a 
+      text-decoration: none
+
+</style>
