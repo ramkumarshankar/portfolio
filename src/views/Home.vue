4bc6cdaca3752d399f70ef74d496b197d3edb6c0
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-08-01 23:31:17 +1000

    fetch linked content from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,22 @@
   <div class="home">
     <Hero v-bind:headline='headline' />
     <div class="container">
-      <h2>I design and build digital products</h2>
-      <h3>I design and build digital products</h3>
-      <h4>I design and build digital products</h4>
-      <p>This is some paragraph copy. Let's see how it looks</p>
+      <h1>My work</h1>
+      <p>I care about design, user experience and clean, maintainable code.</p>
     </div>
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section v-for="(project, index) in projects" :key="'project-' + index">
+      <!-- <template> -->
+        <prismic-rich-text :field="project.primary.title"/>
+        <section v-for="(item, index) in project.items" :key="'project-item-' + index">
+          <!-- <p>{{item.featured_projects}}</p> -->
+          <prismic-link  :field="item.featured_projects">
+            Click here
+          </prismic-link>
+        </section>
+      <!-- </template> -->
+    </section>
+    <!-- <img src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
@@ -22,7 +31,8 @@ export default {
   data () {
     return {
       docID: '',
-      headline: ''
+      headline: '',
+      projects: []
     }
   },
   components: {
@@ -33,8 +43,10 @@ export default {
     getContent (uid) {
       this.$prismic.client.getSingle('homepage').then((document) => {
         console.log(document)
+        console.log(document.data.body)
         this.docID = document.id
         this.headline = this.$prismic.richTextAsPlain(document.data.headline)
+        this.projects = document.data.body
       })
     }
   },
