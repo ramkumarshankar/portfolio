caeb601fce828e2a6d4540e67be9c8efaa67f4fb
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-08-26 14:43:03 +1000

    contact section

---
+++ src/components/ContactSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="contact-block">
+    <h3 class="section-header">I am deeply passionate about the potential of design and technology to create positive, meaningful change.</h3>
+    <button class="primary">Get in touch</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
