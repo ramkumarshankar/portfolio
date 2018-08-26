549ae51d53c6c3c04a3cfc6fcda6bedfae5a2bdd
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-08-27 00:31:49 +1000

    scaffold work page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="work">
     <div class="container">
-      <h1>Projects page</h1>
+      <h1 class="page-headline">Work</h1>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'work'
+}
+</script>
+
+<style lang="stylus" scoped>
+h1.page-headline
+  margin-top: 50px
+</style>
+
