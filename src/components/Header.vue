b943888ea331c69255416d81a27be91fd52380e3
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-08-26 19:36:15 +1000

    header styles changed based on route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
-    <div class="header-inner mx-auto absolute left-0 right-0">
-      <h1 class="site-title">Ramkumar Shankar</h1>
+    <div class="header-inner mx-auto" v-bind:class="[headerClasses]">
+      <router-link :to="{ name: 'home' }"><h1 class="site-title" v-bind:class="[isFixed ? 'light' : '']">Ramkumar Shankar</h1></router-link>
       <nav>
         <ul>
-          <li><a href="#">Work</a></li>
-          <li><a href="#">About</a></li>
-          <li><button class="secondary">Say hello</button></li>
+          <li><router-link v-bind:class="isFixed ? 'light' : ''" :to="{ name: 'work' }">Work</router-link></li>
+          <li><router-link v-bind:class="isFixed ? 'light' : ''" :to="{ name: 'about' }">About</router-link></li>
+          <li><button class="secondary" v-bind:class="isFixed ? 'light' : ''">Say hello</button></li>
         </ul>
       </nav>
     </div>
@@ -15,11 +15,30 @@
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  props: {
+    isFixed: Boolean,
+  },
+  computed: {
+    headerClasses () {
+      if (this.isFixed) {
+        return ['absolute', 'left-0', 'right-0', 'light']
+      } else {
+        return []
+      }
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+.light
+  color: #fff
+
+h1.site-title
+  font-size: 1.414em
+  font-weight: 500
+
 .header-inner
   max-width: 960px
   margin: auto
@@ -37,11 +56,6 @@ export default {
     padding-right: 1.5rem;
     width: 100% !important;
 
-  h1
-    font-size: 1.414em
-    font-weight: 500
-    color: #FFF
-
 ul
   display: inline-block
   list-style-type: none
@@ -50,15 +64,11 @@ ul
     display: inline
     padding-left: 30px
 
-    a
-      text-decoration: none
-      color: #FFF
+  a
+  text-decoration: none
 
-    a:hover
-      box-shadow: inset 0px -1px 0 0 #735290
-      transition: box-shadow 0.2s ease-out;
-
-    button
-      color: #FFF
+  a:hover
+    box-shadow: inset 0px -1px 0 0 #735290
+    transition: box-shadow 0.2s ease-out;
 
 </style>
