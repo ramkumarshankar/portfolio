57407c3eb744c712b5fd6d179cfb3ac1c1e0eb6c
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-08-28 00:05:34 +1000

    start working on the filter menu

---
+++ src/components/FilterMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li><a>All</a></li>
+      <li><a>Creative Coding</a></li>
+      <li><a>Mobile</a></li>
+      <li><a>UI/UX</a></li>
+      <li><a>Research</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterMenu'
+}
+</script>
+
+<style lang="stylus" scoped>
+ul
+
+  li
+    display: inline
+</style>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -2,17 +2,24 @@
   <div class="work">
     <div class="container">
       <h1 class="page-headline">Work</h1>
+      <filter-menu />
     </div>
   </div>
 </template>
 
 <script>
+import FilterMenu from '@/components/FilterMenu.vue'
+
 export default {
-  name: 'work'
+  name: 'work',
+  components: {
+    FilterMenu
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 h1.page-headline
   margin-top: 50px
+  margin-bottom: 20px
 </style>
