a8b98163183e236b99645648be2957112093a8dd
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-08-29 00:37:36 +1000

    work page and filter menu

--- src/components/FilterMenu.vue
+++ src/components/FilterMenu.vue
@@ -1,24 +1,62 @@
 <template>
-  <div>
+  <div class="filterMenu">
     <ul>
-      <li><a>All</a></li>
-      <li><a>Creative Coding</a></li>
-      <li><a>Mobile</a></li>
-      <li><a>UI/UX</a></li>
-      <li><a>Research</a></li>
+      <li v-for="(item, index) in filterItems" v-bind:key="index">
+        <a v-bind:class="[index === activeIndex ? 'active' : '']" v-on:click="updateSelection(index, item)">{{ item }}</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FilterMenu'
+  name: 'FilterMenu',
+  props: {
+    filterItems: Array,
+    activeIndexFromParent: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      activeIndex: null
+    }
+  },
+  methods: {
+    updateSelection (index, item) {
+      this.activeIndex = index
+      this.$emit('filterChanged', item)
+    }
+  },
+  created () {
+    this.activeIndex = this.activeIndexFromParent
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-ul
+@import '../styles/main.styl'
 
+div.filterMenu
+  margin-bottom: 50px
+
+ul
   li
-    display: inline
+    display: inline-block
+    margin-right: 20px
+    margin-bottom: 10px
+    text-transform: uppercase
+    font-weight: 700
+    a
+      padding-bottom: 5px
+      color: #9b9b9b
+      &.active
+        color: $link-color
+        box-shadow: inset 0px -2px 0 0 $link-color
+        transition: box-shadow 0.2s ease-out
+      &:hover
+        color: $link-color
+        box-shadow: inset 0px -2px 0 0 $link-color
+        transition: box-shadow 0.2s ease-out;
 </style>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -2,18 +2,76 @@
   <div class="work">
     <div class="container">
       <h1 class="page-headline">Work</h1>
-      <filter-menu />
+      <filter-menu :filterItems='tagList' v-on:filterChanged='retrieveProjects' />
+      <section class="projects-section">
+        <project-tile v-for="(project, index) in projects" :key="'project-' + index"
+        :title="$prismic.richTextAsPlain(project.data.title)" :description="$prismic.richTextAsPlain(project.data.short_description)" :image="project.data.image" :link="project.data.link" />
+      </section>
     </div>
   </div>
 </template>
 
 <script>
+import ProjectTile from '@/components/ProjectTile.vue'
 import FilterMenu from '@/components/FilterMenu.vue'
 
 export default {
   name: 'work',
   components: {
-    FilterMenu
+    FilterMenu,
+    ProjectTile
+  },
+  data () {
+    return {
+      tagList: null,
+      projects: null
+    }
+  },
+  methods: {
+    getContent () {
+      this.$prismic.client.query(
+        this.$prismic.Predicates.at('document.type', 'project'),
+        { fetch: ['project.title', 'project.short_description', 'project.image', 'project.link'], orderings: '[document.last_publication_date desc]' }
+      ).then((response) => {
+        this.projects = response.results
+        if (!this.tagList) {
+          this.buildTagList()
+        }
+      })
+    },
+    buildTagList () {
+      let rawList = []
+      this.projects.forEach((project, index) => {
+        rawList.push(project.tags)
+      })
+
+      let fullTagList = ['all'].concat(...rawList)
+      fullTagList.forEach((tag, index) => {
+        if (tag === 'featured' || tag === 'Featured') {
+          console.log('inside if')
+          fullTagList.splice(index, 1)
+        }
+      })
+      this.tagList = fullTagList.filter((tag, i, arr) => arr.indexOf(tag) === i)
+    },
+    retrieveProjects (tag) {
+      console.log(tag + ' selected')
+      if (tag === 'all') {
+        this.getContent()
+      } else {
+        this.$prismic.client.query(
+          this.$prismic.Predicates.at('document.tags', [tag]),
+          { fetch: ['project.title', 'project.short_description', 'project.image', 'project.link'], orderings: '[document.last_publication_date desc]' }
+        ).then((response) => {
+          this.projects = response.results
+          // this.buildTagList()
+          // response is the response object, response.results holds the documents
+        })
+      }
+    }
+  },
+  created () {
+    this.getContent()
   }
 }
 </script>
@@ -22,4 +80,8 @@ export default {
 h1.page-headline
   margin-top: 50px
   margin-bottom: 20px
+
+.projects-section
+  display: flex
+  flex-wrap: wrap
 </style>
