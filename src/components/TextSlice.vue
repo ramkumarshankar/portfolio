4036a5faa1fcf01094442905ab91d2615866d3f3
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-09-09 12:36:44 +1000

    start building project page
    text and image slices work

---
+++ src/components/TextSlice.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-slice">
+    <div>
+      <h2>{{ heading }}</h2>
+    </div>
+    <div>
+      <prismic-rich-text :field="body" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextSlice',
+  props: ['heading', 'body']
+}
+</script>
+
+<style lang="stylus" scoped>
+div.text-slice
+  display: grid
+  grid-template-columns: 0.33fr 0.66fr
+  column-gap: 20px
+  grid-column-gap: 20px
+  row-gap: 30px
+  grid-row-gap: 30px
+  margin: 30px 0px
+
+</style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="project">
     <div class="container">
-      <p class="tag">Creative Coding</p>
-      <h1 class="page-headline">Love by Numbers</h1>
+      <span class="tag" v-for="(tag, index) in tags" v-bind:key="index">
+        {{ tag }}
+        <span v-if="index !== tags.length -1">&nbsp;/&nbsp;</span>
+      </span>
+      <h1 class="page-headline">{{ $prismic.richTextAsPlain(this.title) }}</h1>
+      <p>{{ $prismic.richTextAsPlain(this.description) }}</p>
+      <prismic-image class='title-image' :field="titleImage" />
       <section class="projects-section">
         <loading-indicator :loading="loading" />
+        <section v-for="(slice, index) in slices" :key="'slice-' + index">
+          <text-slice v-if="slice.slice_type === 'paragraph'" v-for="(item, index) in slice.items" :key="'textslice-' + index" :heading="$prismic.richTextAsPlain(item.heading)" :body="item.paragraph" />
+          <template v-if="slice.slice_type === 'image'">
+            <template v-for="(item, index) in slice.items">
+              <prismic-image :field="item.image" :key="'imageslice-' + index"/>
+            </template>
+          </template>
+        </section>
       </section>
     </div>
   </div>
@@ -12,20 +25,48 @@
 
 <script>
 import LoadingIndicator from '@/components/LoadingIndicator.vue'
+import TextSlice from '@/components/TextSlice.vue'
 
 export default {
   name: 'Project',
   data () {
     return {
-      loading: false
+      loading: false,
+      tags: '',
+      title: '',
+      description: '',
+      titleImage: '',
+      slices: []
     }
   },
   components: {
-    LoadingIndicator
+    LoadingIndicator,
+    TextSlice
   },
   methods: {
     getContent () {
-      console.log('get content for ' + this.$route.params.slug)
+      let projectUid = this.$route.params.slug
+      console.log('get content for ' + projectUid)
+      this.$prismic.client.getByUID('project', projectUid).then((document) => {
+        // document contains the document content
+        console.log(document)
+        this.title = document.data.title
+        this.description = document.data.short_description
+        this.titleImage = document.data.image
+        // document.title = 'Ramkumar Shankar - Work - ' + this.title
+        this.slices = document.data.body
+        this.buildTagList(document.tags)
+      })
+    },
+    buildTagList (tags) {
+      if (tags) {
+        let displayedTags = tags
+        let index = displayedTags.indexOf('featured')
+        if (index !== -1) {
+          displayedTags.splice(index, 1)
+        }
+        this.tags = displayedTags
+      }
     }
   },
   created () {
@@ -36,13 +77,23 @@ export default {
 
 <style lang="stylus" scoped>
 @import '../styles/main.styl'
+div.project
+  margin-top: 50px
+
+h1.page-headline
+  margin-bottom: 20px
+
 .tag
   margin-top: 5px
-  margin-right: 10px
+  // margin-right: 10px
   font-size: 0.75em
   text-transform: uppercase
   font-weight: 700
   color: $tag-color
   width: auto !important
   border-radius 2px
+
+img.title-image
+  width: 100%
+  object-fit: cover
 </style>
