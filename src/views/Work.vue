0bf21eb956460617a5c096897f10cf257bb8bb38
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-12-01 23:58:23 +1100

    work page does not populate tag list
    + prettier fixes

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -2,28 +2,28 @@
   <div class="work">
     <div class="container">
       <h1 class="page-headline">Work</h1>
-      <filter-menu :filterItems='tagList' v-on:filterChanged='retrieveProjects' />
+      <filter-menu v-on:filterChanged="retrieveProjects"/>
       <section class="projects-section">
-        <loading-indicator :loading="loading" />
-        <projects-grid :projects="projects" />
+        <loading-indicator :loading="loading"/>
+        <projects-grid :projects="projects"/>
       </section>
     </div>
   </div>
 </template>
 
 <script>
-import ProjectsGrid from '@/components/ProjectsGrid.vue'
-import FilterMenu from '@/components/FilterMenu.vue'
-import LoadingIndicator from '@/components/LoadingIndicator.vue'
+import ProjectsGrid from "@/components/ProjectsGrid.vue";
+import FilterMenu from "@/components/FilterMenu.vue";
+import LoadingIndicator from "@/components/LoadingIndicator.vue";
 
 export default {
-  name: 'Work',
+  name: "Work",
   metaInfo: {
     // all titles will be injected into this template
-    titleTemplate: '%s | Work',
+    titleTemplate: "%s | Work",
     meta: [
-      {vmid: 'og:title', content: 'Work'},
-      {vmid: 'og:url', content: 'https://www.ramkumar.me/work/'}
+      { vmid: "og:title", content: "Work" },
+      { vmid: "og:url", content: "https://www.ramkumar.me/work/" }
     ]
   },
   components: {
@@ -31,75 +31,76 @@ export default {
     ProjectsGrid,
     LoadingIndicator
   },
-  data () {
+  data() {
     return {
       loading: false,
-      tagList: null,
       projects: []
-    }
+    };
   },
   methods: {
-    getContent () {
+    getContent() {
       // Start progress bar
-      this.loading = true
-      this.startProgress()
-      this.$prismic.client.query(
-        this.$prismic.Predicates.at('document.type', 'project'),
-        { fetch: ['project.title', 'project.short_description', 'project.image', 'project.link'], orderings: '[document.last_publication_date desc]' }
-      ).then((response) => {
-        this.setProgress(95)
-        this.buildProjectsList(response.results)
-        this.loading = false
-        if (!this.tagList) {
-          this.buildTagList()
-        }
-      })
-    },
-    buildTagList () {
-      let rawList = []
-      this.projects.forEach((project, index) => {
-        rawList.push(project.tags)
-      })
-
-      let fullTagList = ['all'].concat(...rawList)
-      fullTagList.forEach((tag, index) => {
-        if (tag === 'featured' || tag === 'Featured') {
-          fullTagList.splice(index, 1)
-        }
-      })
-      this.tagList = fullTagList.filter((tag, i, arr) => arr.indexOf(tag) === i)
-    },
-    retrieveProjects (tag) {
-      // Initialise progress bar
-      this.setProgress(0)
-      this.loading = true
-      if (tag === 'all') {
-        this.getContent()
-      } else {
-        this.startProgress()
-        this.$prismic.client.query(
-          this.$prismic.Predicates.at('document.tags', [tag]),
-          { fetch: ['project.title', 'project.short_description', 'project.image', 'project.link'], orderings: '[document.last_publication_date desc]' }
-        ).then((response) => {
-          this.setProgress(95)
-          this.buildProjectsList(response.results)
-          this.loading = false
+      this.loading = true;
+      this.startProgress();
+      this.$prismic.client
+        .query(this.$prismic.Predicates.at("document.type", "project"), {
+          fetch: [
+            "project.title",
+            "project.short_description",
+            "project.image",
+            "project.link"
+          ],
+          orderings: "[document.last_publication_date desc]"
         })
+        .then(response => {
+          // console.log(response);
+          this.setProgress(95);
+          this.buildProjectsList(response.results);
+          this.loading = false;
+        });
+    },
+    retrieveProjects(tag) {
+      // Initialise progress bar
+      this.setProgress(0);
+      this.loading = true;
+      if (tag === "all") {
+        this.getContent();
+      } else {
+        this.startProgress();
+        this.$prismic.client
+          .query(this.$prismic.Predicates.at("document.tags", [tag]), {
+            fetch: [
+              "project.title",
+              "project.short_description",
+              "project.image",
+              "project.link"
+            ],
+            orderings: "[document.last_publication_date desc]"
+          })
+          .then(response => {
+            this.setProgress(95);
+            this.buildProjectsList(response.results);
+            this.loading = false;
+          });
       }
     },
-    buildProjectsList (projectsResponse) {
+    buildProjectsList(projectsResponse) {
       // Remove projects that are not in response
       this.projects = this.projects.filter(project => {
-        let indexInArray = projectsResponse.map(projectItem => {
-          return projectItem['id']
-        }).indexOf(project.id)
-        return indexInArray !== -1
-      })
+        let indexInArray = projectsResponse
+          .map(projectItem => {
+            return projectItem["id"];
+          })
+          .indexOf(project.id);
+        return indexInArray !== -1;
+      });
       // Add additional projects to the array
       projectsResponse.forEach((projectItem, index) => {
-        let indexInArray = this.projects.map(project => {
-          return project['id']
-        }).indexOf(projectItem.id)
+        let indexInArray = this.projects
+          .map(project => {
+            return project["id"];
+          })
+          .indexOf(projectItem.id);
         if (indexInArray === -1) {
           this.projects.push({
             id: projectItem.id,
@@ -108,32 +109,34 @@ export default {
             image: projectItem.data.image,
             link: projectItem.data.link,
             tags: projectItem.tags
-          })
+          });
         }
-      })
+      });
     },
-    startProgress () {
-      this.$Progress.start()
+    startProgress() {
+      this.$Progress.start();
     },
-    setProgress (value) {
-      this.$Progress.set(value)
+    setProgress(value) {
+      this.$Progress.set(value);
     },
-    finishProgress () {
-      this.$Progress.finish()
+    finishProgress() {
+      this.$Progress.finish();
     }
   },
-  created () {
-    this.getContent()
+  created() {
+    this.getContent();
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-h1.page-headline
-  margin-top: 50px
-  margin-bottom: 20px
+h1.page-headline {
+  margin-top: 50px;
+  margin-bottom: 20px;
+}
 
-section.projects-section
-  position: relative
-  min-height: 300px
+section.projects-section {
+  position: relative;
+  min-height: 300px;
+}
 </style>
