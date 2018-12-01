26e999d0422db58ee72c027dd979176999c1cc54
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-12-02 00:22:54 +1100

    start work on pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in pages" v-bind:key="index">
+      <a href="#">{{ item.name }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  props: {
+    numPages: Number
+  },
+  computed: {
+    pages() {
+      const range = []
+      for (let i = 1; i <= this.numPages; i++) {
+        range.push({
+          name: i,
+        });
+      }
+      return range
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '../styles/main.styl';
+</style>
