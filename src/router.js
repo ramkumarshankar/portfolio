4468301d0ab6b6cf975cd1ce0dd3d64e2b329970
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-12-01 23:18:56 +1100

    asynchronously load routes when required

--- src/router.js
+++ src/router.js
@@ -1,43 +1,39 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Work from './views/Work.vue'
-import Project from './views/Project.vue'
-import About from './views/About.vue'
-import Contact from './views/Contact.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 const router = new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home,
       props: true
     },
     {
-      path: '/work',
-      name: 'work',
-      component: Work
+      path: "/work",
+      name: "work",
+      component: () => import("./views/Work.vue")
     },
     {
-      path: '/work/:slug',
-      name: 'project',
-      component: Project
+      path: "/work/:slug",
+      name: "project",
+      component: () => import("./views/Project.vue")
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: "/about",
+      name: "about",
+      component: () => import("./views/About.vue")
     },
     {
-      path: '/contact',
-      name: 'contact',
-      component: Contact
+      path: "/contact",
+      name: "contact",
+      component: () => import("./views/Contact.vue")
     }
   ]
-})
+});
 
-export default router
+export default router;
