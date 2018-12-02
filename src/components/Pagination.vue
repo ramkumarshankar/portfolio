f74e50ca6a1ee9ff9d81d0309a0920d0c34c9a11
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-12-02 13:22:14 +1100

    scaffold and style pagination component

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,7 +1,17 @@
 <template>
-  <ul>
-    <li v-for="(item, index) in pages" v-bind:key="index">
+  <ul class="pagination-container">
+    <li class="previous">
+      <a href="#">&#8249;</a>
+    </li>
+    <li
+      :class="'page ' + ((index === currentPage) ? 'active' : '')"
+      v-for="(item, index) in pages"
+      v-bind:key="index"
+    >
       <a href="#">{{ item.name }}</a>
+    </li>
+    <li class="previous">
+      <a href="#">&#8250;</a>
     </li>
   </ul>
 </template>
@@ -10,17 +20,25 @@
 export default {
   name: "Pagination",
   props: {
-    numPages: Number
+    numPages: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      currentPage: 0
+    };
   },
   computed: {
     pages() {
-      const range = []
+      const range = [];
       for (let i = 1; i <= this.numPages; i++) {
         range.push({
-          name: i,
+          name: i
         });
       }
-      return range
+      return range;
     }
   }
 };
@@ -28,4 +46,32 @@ export default {
 
 <style lang="stylus" scoped>
 @import '../styles/main.styl';
+
+ul.pagination-container {
+  text-align: center;
+  display: block;
+  margin: 20px 0px;
+
+  li {
+    padding: 8px 15px;
+    display: inline-block;
+    margin: 0px 10px;
+
+    &.page {
+      border-radius: 2px;
+    }
+
+    a {
+      color: $text-color;
+    }
+
+    &.active {
+      background: $link-color;
+
+      a {
+        color: #FFF;
+      }
+    }
+  }
+}
 </style>
