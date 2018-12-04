519b56b4e6429658778b6e5479619f69886fc39c
Ramkumar Shankar <ramkumarshankr@gmail.com>
2018-12-05 00:17:58 +1100

    prettier fixes

--- src/main.js
+++ src/main.js
@@ -1,41 +1,41 @@
-import Vue from 'vue'
-import PrismicVue from 'prismic-vue'
-import VueProgressBar from 'vue-progressbar'
-import Notifications from 'vue-notification'
-import Meta from 'vue-meta'
+import Vue from "vue";
+import PrismicVue from "prismic-vue";
+import VueProgressBar from "vue-progressbar";
+import Notifications from "vue-notification";
+import Meta from "vue-meta";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import '@ibm/type/css/ibm-type.min.css'
-import 'fractures/dist/fractures.css'
-import 'socicon/css/socicon.css'
+import "@ibm/type/css/ibm-type.min.css";
+import "fractures/dist/fractures.css";
+import "socicon/css/socicon.css";
 
-import linkResolver from './prismic/link-resolver'
+import linkResolver from "./prismic/link-resolver";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(Meta)
+Vue.use(Meta);
 
-const accessToken = process.env.VUE_APP_API_TOKEN
+const accessToken = process.env.VUE_APP_API_TOKEN;
 
 Vue.use(PrismicVue, {
-  endpoint: 'https://ramkumarshankar.cdn.prismic.io/api/v2',
+  endpoint: "https://ramkumarshankar.cdn.prismic.io/api/v2",
   linkResolver,
   apiOptions: {
     accessToken
   }
-})
+});
 
 Vue.use(VueProgressBar, {
-  color: '#735290',
-  thickness: '5px',
+  color: "#735290",
+  thickness: "5px",
   autoFinish: true
-})
+});
 
-Vue.use(Notifications)
+Vue.use(Notifications);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
